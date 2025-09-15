fd26ee7d23d226fbf239ca292dd03abd4411a849
gpro19 <119834015+gpro19@users.noreply.github.com>
2025-09-15 21:06:56 +0700

    Update paket_xut.py

--- paket_xut.py
+++ paket_xut.py
@@ -1,25 +1,22 @@
-import json
-from api_request import send_api_request, get_family
+from api_request import get_family
 from auth_helper import AuthInstance
 
 PACKAGE_FAMILY_CODE = "08a3b1e6-8e78-4e45-a540-b40f06871cfe"
 
 def get_package_xut():
-    """Mengambil dan mengembalikan daftar paket XUT."""
     api_key = AuthInstance.api_key
     tokens = AuthInstance.get_active_tokens()
     if not tokens:
-        print("No active user tokens found.")
         return None
     
     packages = []
     
     data = get_family(api_key, tokens, PACKAGE_FAMILY_CODE)
-    if not data:
+    if not data or not data.get("package_variants"):
         return None
-    
+
     package_variants = data["package_variants"]
-    start_number = 1
+    
     for variant in package_variants:
         for option in variant["package_options"]:
             friendly_name = option["name"]
@@ -30,12 +27,8 @@ def get_package_xut():
                 friendly_name = "Unli Turbo Iflix"
                 
             packages.append({
-                "number": start_number,
                 "name": friendly_name,
                 "price": option["price"],
                 "code": option["package_option_code"]
             })
-            
-            start_number += 1
     return packages
-
