fa177fdc1e6a82f9b7a8db1f628e6980d31b1bb9
gpro19 <119834015+gpro19@users.noreply.github.com>
2025-09-15 21:06:17 +0700

    Update paket_custom_family.py

--- paket_custom_family.py
+++ paket_custom_family.py
@@ -1,9 +1,7 @@
-import json
-from api_request import send_api_request, get_family
+from api_request import get_family
 from auth_helper import AuthInstance
 
 def get_packages_by_family(family_code: str):
-    """Mengambil dan mengembalikan daftar paket berdasarkan kode family."""
     api_key = AuthInstance.api_key
     tokens = AuthInstance.get_active_tokens()
     if not tokens:
@@ -12,21 +10,16 @@ def get_packages_by_family(family_code: str):
     packages = []
     
     data = get_family(api_key, tokens, family_code)
-    if not data:
+    if not data or not data.get("package_variants"):
         return None
     
     package_variants = data["package_variants"]
-    option_number = 1
     
     for variant in package_variants:
         for option in variant["package_options"]:
             packages.append({
-                "number": option_number,
                 "name": option["name"],
                 "price": option["price"],
                 "code": option["package_option_code"]
             })
-            option_number += 1
-            
     return packages
-
