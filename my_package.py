5c84ca696653eeee159bb1998617be93710e50d4
gpro19 <119834015+gpro19@users.noreply.github.com>
2025-09-15 21:07:22 +0700

    Update my_package.py

--- my_package.py
+++ my_package.py
@@ -1,15 +1,11 @@
 from api_request import get_package, send_api_request
 from auth_helper import AuthInstance
 
-def get_my_packages_data():
-    """Mengambil dan mengembalikan data paket dalam bentuk list of dicts."""
+def fetch_my_packages(id_token: str):
     api_key = AuthInstance.api_key
-    tokens = AuthInstance.get_active_tokens()
-    if not tokens:
-        return None
     
-    id_token = tokens.get("id_token")
     path = "api/v8/packages/quota-details"
+    
     payload = {
         "is_enterprise": False,
         "lang": "en",
@@ -18,24 +14,24 @@ def get_my_packages_data():
     
     res = send_api_request(api_key, path, payload, id_token, "POST")
     if res.get("status") != "SUCCESS":
-        print("Failed to fetch packages:", res)
         return None
     
     quotas = res["data"]["quotas"]
-    my_packages = []
-
+    
+    my_packages =[]
     for quota in quotas:
+        quota_code = quota["quota_code"]
         name = quota["name"]
+        family_code = "N/A"
         
-        remaining_quota = "N/A"
-        if "remaining_quota" in quota:
-            remaining_quota = quota["remaining_quota"]
-
+        # Ambil family code
+        package_details = get_package(api_key, {"id_token": id_token}, quota_code)
+        if package_details:
+            family_code = package_details.get("package_family", {}).get("package_family_code", "N/A")
+        
         my_packages.append({
             "name": name,
-            "remaining_quota": remaining_quota,
-            "quota_code": quota["quota_code"],
+            "quota_code": quota_code,
+            "family_code": family_code,
         })
-    
     return my_packages
-
