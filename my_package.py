d71efa8d71482e716f432a37baa590235c3e1c95
gpro19 <119834015+gpro19@users.noreply.github.com>
2025-09-15 21:27:56 +0700

    Update my_package.py

--- my_package.py
+++ my_package.py
@@ -6,33 +6,25 @@ def fetch_my_packages():
     tokens = AuthInstance.get_active_tokens()
     if not tokens:
         return None
-    
     id_token = tokens.get("id_token")
-    
     path = "api/v8/packages/quota-details"
-    
     payload = {
         "is_enterprise": False,
         "lang": "en",
         "family_member_id": ""
     }
-    
     res = send_api_request(api_key, path, payload, id_token, "POST")
     if res.get("status") != "SUCCESS":
         return None
-    
     quotas = res["data"]["quotas"]
-    
     my_packages =[]
     for quota in quotas:
         quota_code = quota["quota_code"]
         name = quota["name"]
         family_code = "N/A"
-        
         package_details = get_package(api_key, tokens, quota_code)
         if package_details:
             family_code = package_details.get("package_family", {}).get("package_family_code", "N/A")
-        
         my_packages.append({
             "name": name,
             "quota_code": quota_code,
